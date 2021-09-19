434abf5f48edf415a7fbadb8b2e0e445b5c22c0a
prakash <prakash8876@gmail.com>
2021-09-19 11:45:04 +0530

    ignoring .class files

--- Core Java/src/com/thread/App.java
+++ Core Java/src/com/thread/App.java
@@ -3,9 +3,6 @@ package com.thread;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Testing");
-		System.out.println("Almost finished");
 	}
 
 }
