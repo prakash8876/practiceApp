39f9db8eeb5d9ced396944453ec24127c42fc49a
prakash <prakash8876@gmail.com>
2021-09-19 12:33:28 +0530

    added whole project

---
+++ Spring/ExceptionHandlingApp/src/main/java/com/exception/main/ExceptionHandlingAppApplication.java
@@ -0,0 +1,13 @@
+package com.exception.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlingAppApplication.class, args);
+	}
+
+}
