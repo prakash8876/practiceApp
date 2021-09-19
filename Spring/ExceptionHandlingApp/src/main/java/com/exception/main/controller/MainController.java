5ebdbcf50e7abfd3c9eea8691857136e09971699
prakash <prakash8876@gmail.com>
2021-09-19 12:36:42 +0530

    added application name

--- Spring/ExceptionHandlingApp/src/main/java/com/exception/main/controller/MainController.java
+++ Spring/ExceptionHandlingApp/src/main/java/com/exception/main/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.exception.main.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 	
+	@Value("${app.name}")
+	String applicationName;
+	
 	@GetMapping
 	public String greetings() {
-		return "Welcome to Spring Boot Application!";
+		return "Welcome to " + applicationName;
 	}
 	
 	@GetMapping(value= "/{name}")
