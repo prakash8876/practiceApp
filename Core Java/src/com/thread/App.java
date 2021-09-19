1bf55c2665df8a5dc8d6d30adcac157706919688
prakash <prakash8876@gmail.com>
2021-09-19 11:09:32 +0530

    # WARNING: head commit changed in the meantime
    initial commit

---
+++ Core Java/src/com/thread/App.java
@@ -0,0 +1,11 @@
+package com.thread;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing");
+		System.out.println("Almost finished");
+	}
+
+}
---
+++ Core Java/src/com/thread/ConcurrentExample.java
@@ -0,0 +1,10 @@
+package com.thread;
+
+public class ConcurrentExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
