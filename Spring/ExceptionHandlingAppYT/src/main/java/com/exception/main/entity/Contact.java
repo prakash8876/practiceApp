d7218e7a1e5e0772ff6cfd987e6026aac9ddf1fb
prakash <prakash8876@gmail.com>
2021-10-09 11:40:00 +0530

    added project lombok

--- Spring/ExceptionHandlingAppYT/src/main/java/com/exception/main/entity/Contact.java
+++ Spring/ExceptionHandlingAppYT/src/main/java/com/exception/main/entity/Contact.java
@@ -8,7 +8,15 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "contact")
 public class Contact {
@@ -32,8 +40,6 @@ public class Contact {
 	@Column(name = "phone")
 	private String phoneNo;
 
-	public Contact() {}
-
 	public Contact(String firstName, String lastName, String email, String phoneNo) {
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -41,45 +47,6 @@ public class Contact {
 		this.phoneNo = phoneNo;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhoneNo() {
-		return phoneNo;
-	}
-
-	public void setPhoneNo(String phoneNo) {
-		this.phoneNo = phoneNo;
-	}
 
 	@Override
 	public String toString() {
