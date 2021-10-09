c3de2979ffa477acf054f94ecc333030492003bf
prakash <prakash8876@gmail.com>
2021-10-09 17:35:56 +0530

    response status to exception class

--- Spring/ExceptionHandlingAppYT/src/main/java/com/exception/main/exception/RecordNotFoundException.java
+++ Spring/ExceptionHandlingAppYT/src/main/java/com/exception/main/exception/RecordNotFoundException.java
@@ -1,5 +1,9 @@
 package com.exception.main.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class RecordNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 467072583441737163L;
